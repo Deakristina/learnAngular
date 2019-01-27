f3b6e09f7840f7ba46ff73b203bc91ab75121c85
Deakristina <deakristina@gmail.com>
2019-01-27 15:40:33 +0100

    create & count likes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'like-app';
+	title = 'like-app';
+	tweet = {
+		body: "Tweet's body..",
+		isLike: false,
+		likesCount: 0
+	};
 }
